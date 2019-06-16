3fab1f3c519e34452f724c30ec55ed280ce2a9b0
7SergSmith7 <serg.kuznetsov89@gmail.com>
2019-06-16 13:23:27 +0300

    Первый код

---
+++ Задача с Кредитным рейтингом/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество клиентов:");
+            string ns = Console.ReadLine();
+            int n = Convert.ToInt32(ns);
+            Console.WriteLine("Количество клиентв равно " + n);
+            Random rand = new Random();
+            double[] clientsRanc = new double[n];
+            double [] clientsWant = new double[n];
+            bool[] clientAnswer = new bool[n];
+            for (int i = 0; i < n; i++)
+            {
+                clientsRanc[i] = rand.Next(1, 10) + rand.NextDouble();
+                clientsRanc[i] = Math.Round(clientsRanc[i], 2);
+                int numClient = i + 1;
+                Console.WriteLine("Клиент " + numClient + " имеет рейтинг " + clientsRanc[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int numClient = i + 1;
+                clientsWant[i] = rand.Next(1, 20000);
+                Console.WriteLine("Клиент " + numClient + " желает получить " + clientsWant[i] + " грн");
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                double creditLim = clientsRanc[i] * 2000;
+                int numClient = i + 1;
+                if (creditLim >= clientsWant[i])
+                {
+                    Console.WriteLine("Клиент " + numClient + " может получить кредит.");
+                }
+                else
+                {
+                    Console.WriteLine("Клиент " + numClient + " НЕ может получить кредит.");
+                }
+            }
+           
+        }
+    }
+}
---
+++ Калькулятор/Program_calc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Калькулятор v.0.1");
+                Console.WriteLine("Введите число 1");
+                string num1s = Console.ReadLine();
+                double num1 = Convert.ToDouble(num1s);
+                Console.WriteLine("Введите число 2");
+                string num2s = Console.ReadLine();
+                double num2 = Convert.ToDouble(num2s);
+                Console.WriteLine("Выбирете операцию (+, -, /, *)");
+                string act = Console.ReadLine();
+                double result;
+                switch (act)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine(num1 + " + " + num2 + " = " + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine(num1 + " - " + num2 + " = " + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine(num1 + " * " + num2 + " = " + result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("На ноль делить нельзя");
+                        }
+                        else
+                        {
+                            Console.WriteLine(num1 + " / " + num2 + " = " + result);
+                        }
+                        break;
+                }
+                Console.WriteLine("Повторить? Да(y) / Нет(n)");
+            } while (Console.ReadLine() == "y");
+        }
+    }
+}
