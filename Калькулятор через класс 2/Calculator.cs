4b87fb1bac6be8d259fd1db4e21245b846c3df21
7SergSmith7 <serg.kuznetsov89@gmail.com>
2019-11-27 23:47:50 +0200

    Новый калькулятор

---
+++ Калькулятор через класс 2/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multy(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Div(double a, double b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ Калькулятор через класс 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            char act;
+            Console.WriteLine("Enter first element:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second element:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter action:");
+            act =Convert.ToChar( Console.ReadLine());
+
+            switch(act)
+            {
+                case '+':
+                    Console.WriteLine(Calculator.Add(a, b)); 
+                    break;
+                case '-':
+                    Console.WriteLine(Calculator.Minus(a, b));
+                    break;
+                case '*':
+                    Console.WriteLine(Calculator.Multy(a, b));
+                    break;
+                case '/':
+                    Console.WriteLine(Calculator.Div(a, b));
+                    break;
+                default:
+                    Console.WriteLine("Enter rigth sign.");
+                    break;
+
+            }
+        }
+    }
+}
